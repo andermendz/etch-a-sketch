1d1da9388ed62f5f86880da6947c9179f3885643
andermendz <andersonmendz19@gmail.com>
2023-01-07 22:22:41 -0500

    functional prototype javascript done, todo responsive grid

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
-const boxes = document.querySelectorAll('.box');
+const container = document.querySelector("#container");
+const generate = document.querySelector("#generate");
 
-boxes.forEach((box) => {
-   box.addEventListener( 'mousemove', () =>{
-        console.log('box clicked')
-        box.setAttribute('style', 'background-color:black;');
-        
-    })
+generate.addEventListener("click", () =>{
+    generator()
+    reactions();
 })
+
+
+function reactions() {
+  const boxes = document.querySelectorAll(".box");
+
+  boxes.forEach((box) => {1
+    box.addEventListener("mousemove", () => {
+      console.log("box touched");
+      box.setAttribute("style", "background-color:black;");
+    });
+  });
+}
+
+function generator() {
+ while (container.lastChild) {
+    container.removeChild(container.lastChild);
+
+ }
+    
+var ratio = prompt("How many boxes and lines?")
+  for (i = 0; i < ratio; i++) {
+    const line = document.createElement("div");
+    line.setAttribute("class", "line");
+  
+    for (j = 0; j < ratio; j++) {
+      const box = document.createElement("div");
+      box.setAttribute("class", "box");
+      line.appendChild(box);
+    }
+    container.appendChild(line);
+  }
+}
