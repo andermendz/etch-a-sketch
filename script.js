000ce40b597f4d9fd1b8e6572459a8f1d841bea2
andermendz <andersonmendz19@gmail.com>
2023-01-09 11:35:48 -0500

    conditionals and adaptative grid done

--- script.js
+++ script.js
@@ -24,18 +24,25 @@ function generator() {
 
  }
     
-var ratio = prompt("How many boxes and lines?")
-var hline = 100 / ratio;
-console.log(hline)
-  for (i = 0; i < ratio; i++) {
-    const line = document.createElement("div");
-    line.setAttribute("class", "line");
-    line.style.height = `${hline}%`  
-    for (j = 0; j < ratio; j++) {
-      const box = document.createElement("div");
-      box.setAttribute("class", "box");
-      line.appendChild(box);
+var ratio = prompt("How many boxes and lines from 1 - 100?")
+
+while (ratio > 100 || ratio <= 0) {
+  var ratio = prompt("How many boxes and lines from 1 - 100?")
+} 
+if (ratio > 0 || ratio <= 100) {
+  var hline = 100 / ratio;
+  console.log(hline)
+    for (i = 0; i < ratio; i++) {
+      const line = document.createElement("div");
+      line.setAttribute("class", "line");
+      line.style.height = `${hline}%`  
+      for (j = 0; j < ratio; j++) {
+        const box = document.createElement("div");
+        box.setAttribute("class", "box");
+        line.appendChild(box);
+      }
+      container.appendChild(line);
     }
-    container.appendChild(line);
   }
 }
+
