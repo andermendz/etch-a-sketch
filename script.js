6720b45d37501a7ae17722a466bab9c86f477450
andermendz <andersonmendz19@gmail.com>
2023-01-09 12:04:35 -0500

    added comments

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const container = document.querySelector("#container");
 const generate = document.querySelector("#generate");
 
-generate.addEventListener("click", () =>{
-    generator()
-    reactions();
-})
+generate.addEventListener("click", () => { // call the functions
+  generator();
+  reactions();
+});
 
-
-function reactions() {
+function reactions() { // added reactions
   const boxes = document.querySelectorAll(".box");
 
-  boxes.forEach((box) => {1
-    box.addEventListener("mousemove", () => {
+  boxes.forEach((box) => {
+    1;
+    box.addEventListener("mousemove", (e) => {
       console.log("box touched");
       box.setAttribute("style", "background-color:black;");
     });
@@ -19,30 +19,28 @@ function reactions() {
 }
 
 function generator() {
- while (container.lastChild) {
+  while (container.lastChild) {
     container.removeChild(container.lastChild);
+  }
+// ratio conditionals
+  var ratio = prompt("How many boxes and lines from 1 - 100?");
 
- }
-    
-var ratio = prompt("How many boxes and lines from 1 - 100?")
-
-while (ratio > 100 || ratio <= 0) {
-  var ratio = prompt("How many boxes and lines from 1 - 100?")
-} 
-if (ratio > 0 || ratio <= 100) {
-  var hline = 100 / ratio;
-  console.log(hline)
-    for (i = 0; i < ratio; i++) {
+  while (ratio > 100 || ratio <= 0) {
+    var ratio = prompt("How many boxes and lines from 1 - 100?");
+  }
+  if (ratio > 0 || ratio <= 100) {
+    var hline = 100 / ratio; // the 100 percent of the heigh / amount of lines = the heigh of each line
+    console.log(hline);
+    for (i = 0; i < ratio; i++) { // generate the same amount of lines and boxes
       const line = document.createElement("div");
       line.setAttribute("class", "line");
-      line.style.height = `${hline}%`  
+      line.style.height = `${hline}%`;
       for (j = 0; j < ratio; j++) {
         const box = document.createElement("div");
         box.setAttribute("class", "box");
         line.appendChild(box);
-      }
+      }// add the generated lines to the container
       container.appendChild(line);
     }
   }
 }
-
